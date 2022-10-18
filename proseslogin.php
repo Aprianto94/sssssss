a1b65db03937a07f92e68a0a9ab5f760dd7ef30b
Tio aprianto-12 RPL <110009159+Aprianto94@users.noreply.github.com>
2022-10-18 11:04:52 +0700

    Add files via upload

--- proseslogin.php
+++ proseslogin.php
@@ -9,7 +9,9 @@
 
     $query = $koneksi->query("select * from akun where nik='$nik' AND nama_lengkap='$nama_lengkap'")->fetchAll();
     
-    if (count($query) > 0){
+    if ($query){
+        $_SESSION["nik"] = $_REQUEST['nik'];
+        $_SESSION["nama_lengkap"] = $_REQUEST['nama_lengkap'];
         header("Location:index.php");
     }
     else{
